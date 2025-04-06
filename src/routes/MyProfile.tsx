d268eb379de7af8140c06661cb3c260bcb1a2509
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-04-07 05:57:15 +0700

    style: improving UI and spacing for my profile and profile page

--- src/routes/MyProfile.tsx
+++ src/routes/MyProfile.tsx
@@ -1,11 +1,11 @@
+import { NavLink } from "react-router-dom";
 import { Avatar } from "@/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { FaArrowLeft, FaRegHeart } from "react-icons/fa";
-import { LiaComment } from "react-icons/lia";
-import { NavLink } from "react-router-dom";
-import banner from "@/assets/image/abstract-polygonal-banner-background.jpg";
+import { TbArrowLeft } from "react-icons/tb";
+import { TbHeart } from "react-icons/tb";
+// import { TbHeartFilled } from "react-icons/tb";
+import { TbMessage2 } from "react-icons/tb";
 import { Button } from "@/components/ui/button";
-import imagePost from "@/assets/image/white-blossom.png";
 import {
   Dialog,
   DialogContent,
@@ -14,8 +14,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { BiImageAdd } from "react-icons/bi";
+import banner from "@/assets/image/abstract-polygonal-banner-background.jpg";
+import imagePost from "@/assets/image/white-blossom.png";
 
 function MyProfile() {
   const image1: string =
@@ -23,29 +23,31 @@ function MyProfile() {
 
   return (
     <section>
-      <div className="grid grid-cols-1 border-collapse">
-        <div className="flex flex-col pt-10 px-5 border-2 border-[#2c2c2c] gap-5">
-          <div className="flex items-center text-3xl gap-3">
-            <NavLink to={"/"}>
-              <FaArrowLeft className="text-2xl" />
-            </NavLink>
-            <h1 className="font-medium">Stella Audhina</h1>
-          </div>
-          <img src={banner} alt="" className="rounded-lg h-40 w-full" />
+      {/* Title Page */}
+      <div className="pt-10 px-5 pb-2 flex items-center text-3xl gap-[10px] opacity-80">
+        <NavLink to={"/"}>
+          <TbArrowLeft className="w-7 h-7 stroke-1" />
+        </NavLink>
+        <h1 className="font-bold text-3xl">Stella Audhina</h1>
+      </div>
+      <div className="grid grid-cols-1 border-collapse gap-3">
+        {/* Profile Info */}
+        <div className="top-30 flex flex-col px-5 gap-5">
+          <img src={banner} alt="" className="rounded-lg h-35 w-full" />
           <div className="flex justify-between items-end w-full">
             <div>
-              <Avatar className="w-20 h-20 ms-4 -mt-24 border-4 border-[#262626]">
+              <Avatar className="size-20 ms-4 -mt-24 border-4 border-[#262626]">
                 <AvatarImage src="https://github.com/shadcn.png" />
               </Avatar>
             </div>
             <div className="">
               <Dialog>
                 <DialogTrigger asChild>
-                  <Button variant={"outline"} className="px-5 flex-1 w-full">
+                  <button className="px-4 py-2 flex-1 w-full text-sm font-bold rounded-full border-1 border-[#FFFFFF]">
                     Edit Profile
-                  </Button>
+                  </button>
                 </DialogTrigger>
-                <DialogContent className="absolute top-100 w-[60%] bg-[#1d1d1d] text-[#e8e8e8] border-none rounded-2xl pt-10 px-5 gap-10">
+                <DialogContent className="absolute top-90 size-150 bg-[#1d1d1d] text-[#e8e8e8] border-none rounded-2xl pt-10 px-5 gap-5 focus:outline-none focus:border-none">
                   <DialogHeader>
                     <DialogTitle>Edit profile</DialogTitle>
                   </DialogHeader>
@@ -54,7 +56,7 @@ function MyProfile() {
                     <AvatarImage src="https://github.com/shadcn.png" />
                   </Avatar>
                   {/* <BiImageAdd /> */}
-                  <div className="flex flex-col gap-4 items-start px-5">
+                  <div className="flex flex-col gap-4 items-start">
                     <input
                       className="w-full border-1 border-[#e8e8e8]/50 p-2 text-[#e8e8e8] rounded-sm focus:outline-2 focus:outline-white"
                       type="text"
@@ -82,41 +84,47 @@ function MyProfile() {
               </Dialog>
             </div>
           </div>
-          <div>
-            <div className="flex flex-col gap-3">
-              <h1 className="text-3xl font-semibold">Stella Audhina</h1>
-              <p className="text-[#e8e8e8]/50">@audhinafh</p>
-              <p className="text-lg">
-                picked over by the worms, and weird fishes.
-              </p>
-              <div className="flex gap-2">
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <h1 className="text-2xl font-bold">Stella Audhina</h1>
+              <p className="text-[#909090] text-sm">@audhinafh</p>
+              <p>picked over by the worms, and weird fishes.</p>
+              <div className="flex gap-2 text-base">
                 <p>291</p>
-                <span className="text-[#e8e8e8]/50">following</span>
+                <span className="text-[#909090]">following</span>
                 <p>23</p>
-                <span className="text-[#e8e8e8]/50">followers</span>
+                <span className="text-[#909090]">followers</span>
               </div>
             </div>
           </div>
-          <div className="flex justify-between text-center">
-            <button className="w-full border-3 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
+        </div>
+
+        {/* Tabs */}
+        <div className="flex justify-between text-center font-medium gap-[10px] border-b-1 border-[#2c2c2c]">
+          <div className="px-5 w-full">
+            <button className="w-full py-2 border-4 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
               All Post
             </button>
-            <button className="w-full border-3 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
+          </div>
+          <div className="px-5 w-full">
+            <button className="w-full py-2 border-4 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
               Media
             </button>
           </div>
         </div>
-        <div className="flex flex-col py-5 px-5 border-2 border-[#2c2c2c] gap-8">
-          <article className="flex gap-5 items-start">
-            {/* <Avatar className="w-10 h-10">
+
+        {/* List Post */}
+        <div className="flex flex-col">
+          <article className="flex gap-5 items-start py-4 px-5">
+            <Avatar className="w-10 h-10">
               <AvatarImage src="https://github.com/shadcn.png" />
             </Avatar>
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-1.5 ">
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="flex gap-1">
                 <p className="font-bold">Indah Pra Karya</p>
-                <span className="text-[#e8e8e8]/50">@indahpra</span>
-                <span className="text-[#e8e8e8]/50">•</span>
-                <span className="text-[#e8e8e8]/50">4h</span>
+                <span className="text-[#909090]">@indahpra</span>
+                <span className="text-[#909090]">•</span>
+                <span className="text-[#909090]">4h</span>
               </div>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in
@@ -126,22 +134,63 @@ function MyProfile() {
                 reprehenderit vero distinctio veniam eaque voluptatibus magni
                 quidem tenetur iste repellendus. Nostrum est ipsa doloremque.
               </p>
-              <img src={imagePost} alt="" className="rounded-lg w-[70%]" />
-              <div className="flex gap-3 text-[#e8e8e8]/50">
-                <div className="flex gap-2">
-                  <button className="text-2xl">
-                    <FaRegHeart />
+              <div className="flex gap-3 text-[#909090]">
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbHeart className="size-6" />
                   </button>
                   <span>36</span>
                 </div>
-                <div className="flex gap-2">
-                  <button className="text-2xl">
-                    <LiaComment />
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbMessage2 className="size-6" />
                   </button>
                   <span>381 Replies</span>
                 </div>
               </div>
-            </div> */}
+            </div>
+          </article>
+          <article className="flex gap-5 items-start py-4 px-5">
+            <Avatar className="w-10 h-10">
+              <AvatarImage src="https://github.com/shadcn.png" />
+            </Avatar>
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="flex gap-1">
+                <p className="font-bold">Indah Pra Karya</p>
+                <span className="text-[#909090]">@indahpra</span>
+                <span className="text-[#909090]">•</span>
+                <span className="text-[#909090]">4h</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in
+                quae fugiat perferendis? Cum, officia perspiciatis. Ducimus
+                alias ratione similique pariatur tenetur eaque! Saepe id sed
+                unde, ut rem corrupti, ipsam quae quibusdam magnam, consequuntur
+                reprehenderit vero distinctio veniam eaque voluptatibus magni
+                quidem tenetur iste repellendus. Nostrum est ipsa doloremque.
+              </p>
+              <img
+                src={imagePost}
+                alt=""
+                className="rounded-md w-[70%] min-w-50 max-w-100"
+              />
+              <div className="flex gap-3 text-[#909090]">
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbHeart className="size-6" />
+                  </button>
+                  <span>36</span>
+                </div>
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbMessage2 className="size-6" />
+                  </button>
+                  <span>381 Replies</span>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article className="flex gap-5 items-start py-4 px-5">
             <div className="grid grid-cols-3 gap-2 w-full">
               <div className="">
                 <img src={image1} alt="" className="rounded-lg h-50" />
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,9 +1,8 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { FaArrowLeft } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import imagePost from "@/assets/image/white-blossom.png";
 import banner from "@/assets/image/abstract-polygonal-banner-background.jpg";
+import { TbArrowLeft, TbHeart, TbMessage2 } from "react-icons/tb";
 
 function Profile() {
   const image1: string =
@@ -11,62 +10,70 @@ function Profile() {
 
   return (
     <section>
-      <div className="grid grid-cols-1 border-collapse">
-        <div className="flex flex-col pt-10 px-5 border-2 border-[#2c2c2c] gap-5">
-          <div className="flex items-center text-3xl gap-3">
-            <NavLink to={"/"}>
-              <FaArrowLeft className="text-2xl" />
-            </NavLink>
-            <h1 className="font-medium">Naveen Singh</h1>
-          </div>
-          <img src={banner} alt="" className="rounded-lg h-40 w-full" />
+      {/* Title Page */}
+      <div className="pt-10 px-5 pb-2 flex items-center text-3xl gap-[10px] opacity-80">
+        <NavLink to={"/"}>
+          <TbArrowLeft className="w-7 h-7 stroke-1" />
+        </NavLink>
+        <h1 className="font-bold text-3xl">Naveen Singh</h1>
+      </div>
+      <div className="grid grid-cols-1 border-collapse gap-3">
+        {/* Profile Info */}
+        <div className="top-30 flex flex-col px-5 gap-5">
+          <img src={banner} alt="" className="rounded-lg h-35 w-full" />
           <div className="flex justify-between items-end w-full">
             <div>
-              <Avatar className="w-20 h-20 ms-4 -mt-24 border-4 border-[#262626]">
+              <Avatar className="size-20 ms-4 -mt-24 border-4 border-[#262626]">
                 <AvatarImage src="https://github.com/shadcn.png" />
               </Avatar>
             </div>
             <div className="">
-              <Button variant={"outline"} className="px-5 flex-1 w-full">
+              <button className="px-4 py-2 flex-1 w-full text-sm font-bold rounded-full border-1 border-[#FFFFFF]">
                 Follow
-              </Button>
+              </button>
             </div>
           </div>
-          <div>
-            <div className="flex flex-col gap-3">
-              <h1 className="text-3xl font-semibold">Stella Audhina</h1>
-              <p className="text-[#e8e8e8]/50">@audhinafh</p>
-              <p className="text-lg">
-                picked over by the worms, and weird fishes.
-              </p>
-              <div className="flex gap-2">
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <h1 className="text-2xl font-bold">Stella Audhina</h1>
+              <p className="text-[#909090] text-sm">@audhinafh</p>
+              <p>picked over by the worms, and weird fishes.</p>
+              <div className="flex gap-2 text-base">
                 <p>291</p>
-                <span className="text-[#e8e8e8]/50">following</span>
+                <span className="text-[#909090]">following</span>
                 <p>23</p>
-                <span className="text-[#e8e8e8]/50">followers</span>
+                <span className="text-[#909090]">followers</span>
               </div>
             </div>
           </div>
-          <div className="flex justify-between text-center">
-            <button className="w-full border-3 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
+        </div>
+
+        {/* Tabs */}
+        <div className="flex justify-between text-center font-medium gap-[10px] border-b-1 border-[#2c2c2c]">
+          <div className="px-5 w-full">
+            <button className="w-full py-2 border-4 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
               All Post
             </button>
-            <button className="w-full border-3 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
+          </div>
+          <div className="px-5 w-full">
+            <button className="w-full py-2 border-4 border-[#1d1d1d] hover:border-b-[#04a41e] active:border-b-[#04a41e]">
               Media
             </button>
           </div>
         </div>
-        <div className="flex flex-col py-5 px-5 border-2 border-[#2c2c2c] gap-8">
-          <article className="flex gap-5 items-start">
-            {/* <Avatar className="w-10 h-10">
+
+        {/* List Post */}
+        <div className="flex flex-col">
+          <article className="flex gap-5 items-start py-4 px-5">
+            <Avatar className="w-10 h-10">
               <AvatarImage src="https://github.com/shadcn.png" />
             </Avatar>
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-1.5 ">
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="flex gap-1">
                 <p className="font-bold">Indah Pra Karya</p>
-                <span className="text-[#e8e8e8]/50">@indahpra</span>
-                <span className="text-[#e8e8e8]/50">•</span>
-                <span className="text-[#e8e8e8]/50">4h</span>
+                <span className="text-[#909090]">@indahpra</span>
+                <span className="text-[#909090]">•</span>
+                <span className="text-[#909090]">4h</span>
               </div>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in
@@ -76,22 +83,63 @@ function Profile() {
                 reprehenderit vero distinctio veniam eaque voluptatibus magni
                 quidem tenetur iste repellendus. Nostrum est ipsa doloremque.
               </p>
-              <img src={imagePost} alt="" className="rounded-lg w-[70%]" />
-              <div className="flex gap-3 text-[#e8e8e8]/50">
-                <div className="flex gap-2">
-                  <button className="text-2xl">
-                    <FaRegHeart />
+              <div className="flex gap-3 text-[#909090]">
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbHeart className="size-6" />
                   </button>
                   <span>36</span>
                 </div>
-                <div className="flex gap-2">
-                  <button className="text-2xl">
-                    <LiaComment />
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbMessage2 className="size-6" />
                   </button>
                   <span>381 Replies</span>
                 </div>
               </div>
-            </div> */}
+            </div>
+          </article>
+          <article className="flex gap-5 items-start py-4 px-5">
+            <Avatar className="w-10 h-10">
+              <AvatarImage src="https://github.com/shadcn.png" />
+            </Avatar>
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="flex gap-1">
+                <p className="font-bold">Indah Pra Karya</p>
+                <span className="text-[#909090]">@indahpra</span>
+                <span className="text-[#909090]">•</span>
+                <span className="text-[#909090]">4h</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in
+                quae fugiat perferendis? Cum, officia perspiciatis. Ducimus
+                alias ratione similique pariatur tenetur eaque! Saepe id sed
+                unde, ut rem corrupti, ipsam quae quibusdam magnam, consequuntur
+                reprehenderit vero distinctio veniam eaque voluptatibus magni
+                quidem tenetur iste repellendus. Nostrum est ipsa doloremque.
+              </p>
+              <img
+                src={imagePost}
+                alt=""
+                className="rounded-md w-[70%] min-w-50 max-w-100"
+              />
+              <div className="flex gap-3 text-[#909090]">
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbHeart className="size-6" />
+                  </button>
+                  <span>36</span>
+                </div>
+                <div className="flex gap-2 py-1 items-center">
+                  <button className="">
+                    <TbMessage2 className="size-6" />
+                  </button>
+                  <span>381 Replies</span>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article className="flex gap-5 items-start py-4 px-5">
             <div className="grid grid-cols-3 gap-2 w-full">
               <div className="">
                 <img src={image1} alt="" className="rounded-lg h-50" />
