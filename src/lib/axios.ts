4cd83bfb53fbf403d6638f22136f5318acd46e87
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-07-07 15:21:30 +0700

    feat: add environment variable

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseUrl = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: `${baseUrl}/api/v1`,
   withCredentials: true,
 });
 
