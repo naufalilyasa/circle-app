607a7f768da3c6283672e6f3c65db35815321cad
Naufal Ilyasa <57007555+naufalilyasa@users.noreply.github.com>
2025-07-07 16:16:18 +0700

    fix: Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: `${baseUrl}/api/v1`,
+  baseURL: `${baseUrl}api/v1`,
   withCredentials: true,
 });
 
