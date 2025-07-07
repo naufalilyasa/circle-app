f72d2d056f6930da2e9c45ed2c43c7776a9807b1
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-07-07 17:46:40 +0700

    fix: update

--- src/routes/_privateLayout.tsx
+++ src/routes/_privateLayout.tsx
@@ -1,9 +1,9 @@
 import RightBar from "@/routes/-components/RightBar";
-import { Outlet } from "@tanstack/react-router";
+import { Navigate, Outlet } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { getMeFn } from "@/api/auth";
 import { Skeleton } from "@/components/ui/skeleton";
-// import { useCookies } from "react-cookie";
+import { useCookies } from "react-cookie";
 import LeftBar from "./-components/LeftBar";
 import { Menu } from "lucide-react";
 import { useState } from "react";
@@ -13,7 +13,7 @@ export const Route = createFileRoute({
 });
 
 function MainLayout() {
-  // const [cookies] = useCookies(["logged_in", "access_token"]);
+  const [cookies] = useCookies(["logged_in", "access_token"]);
   const [isOpen, setIsOpen] = useState(false);
 
   const { isLoading } = useQuery({
@@ -45,24 +45,25 @@ function MainLayout() {
         // isError ? (
         //   <Navigate to="/login" replace />
         // ) :
-        //  !cookies.logged_in ? (
-        //   <Navigate to="/login" replace />
-        // ) : (
-        <>
-          {/* Overlay */}
-          {isOpen && (
-            <div className="fixed inset-0 z-40" onClick={toggleSidebar} />
-          )}
-          <LeftBar isOpen={isOpen} />
-          <section className="lg:flex-6/12 md:w-[65%] mx-auto">
-            <button onClick={toggleSidebar} className="md:hidden ps-4 mt-4">
-              <Menu size={32} />
-            </button>
-            <Outlet />
-          </section>
-          <RightBar />
-        </>
-        // )
+        !cookies.logged_in ? (
+          <Navigate to="/login" replace />
+        ) : (
+          <>
+            {/* Overlay */}
+            {isOpen && (
+              <div className="fixed inset-0 z-40" onClick={toggleSidebar} />
+            )}
+            <LeftBar isOpen={isOpen} />
+            <section className="lg:flex-6/12 md:w-[65%] mx-auto">
+              <button onClick={toggleSidebar} className="md:hidden ps-4 mt-4">
+                <Menu size={32} />
+              </button>
+              <Outlet />
+            </section>
+            <RightBar />
+          </>
+          // )
+        )
       }
     </main>
   );
